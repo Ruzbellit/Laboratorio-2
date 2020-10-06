6fe5b320b03eb42fbe14cb764b5ae77913b3fead
dannachi <daniel.rodriguez.sanchez@correounivalle.edu.co>
2020-10-06 11:15:48 -0500

    Se realizan las funciones mostrarAyuda y mostrarEstadisticas

--- src/concentresejuego/ConcentreseJuego.java
+++ src/concentresejuego/ConcentreseJuego.java
@@ -24,5 +24,34 @@ public class ConcentreseJuego {
       String estadisticas;
       
     
-      
-}
+    public String mostrarAyuda()
+    {
+        return "CONCENTRCE"+"\n"+"Saludos invocador, "
+                + "Primero debe seleccionar la tematica del juego y presionar el botón de Iniciar Juego, "
+                + "una vez inicie se le mostrara por un tiempo de cuatro (4) segundos la distribución de las cartas "
+                + "dispondra de tres (3) intentos para encontrar todas las parejas de cartas"+"\n"
+                +"Mucha suerte! c:";
+    }
+    
+    public String mostrarEstadisticas()
+    {
+        return "Estadisticas de Jugadas\n"+"\n"
+                +"Cantidad de juegos realizados: "+ juegosTotales +"\n"
+                + "Porcentaje de juegos ganados: "+ ((juegosGanados/juegosTotales)*100) +"%\n"
+                + "Porcentaje de juegos perdidos "+ (((juegosTotales - juegosGanados)/juegosTotales)*100) +"%\n"
+                ;
+                
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}   
+
