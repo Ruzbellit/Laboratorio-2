6af392ad6cbc0b7c55c2c3b794b5acda7739eb52
Christian Villanueva Paez <christian.villanueva@correounivalle.edu.co>
2020-10-18 00:01:37 -0500

    Documentacion del archivo ConcentreseJuego.java

--- src/concentresejuego/ConcentreseJuego.java
+++ src/concentresejuego/ConcentreseJuego.java
@@ -8,31 +8,41 @@ package concentresejuego;
 import java.util.*;
 
 /**
- *
- * @author ruzbe
+ * Clase que ejecuta la logica del juego Concentrese
  */
 public class ConcentreseJuego {
 
+    //Variables del estado del juego
     int vidas;
     int fallos;
     int aciertos;
-    int matriz[][] = new int[4][4];
-    boolean matrizDestapar[][];
-    boolean casillaDestapada;
-    boolean parCasillasDestapadas;
+    
+    int matriz[][] = new int[4][4]; //Matriz que guarda un numero correpondiente a la imagen
+    boolean matrizDestapar[][]; //Matriz que indica si la casilla esta tapada o destapada
+    boolean casillaDestapada;   //Indica si ya se ha destapado 1 casilla
+    boolean parCasillasDestapadas;  //Indica si ya se han destapado 2 casillas diferentes
+    
+    //Guarda las coordenadas en la matriz de las 2 casillas destapadas
     int casillaDestapada1X;
     int casillaDestapada1Y;
     int casillaDestapada2X;
     int casillaDestapada2Y;
+    
+    //Variables para el conteo de juegos completados
     int juegosTotales;
     int juegosGanados;
-    String estadisticas;
 
+    /**
+     * Inicializa el objeto y el conteo de los juegos totales y perdidos
+     */
     public ConcentreseJuego() {
         juegosTotales = 0;
         juegosGanados = 0;
     }
 
+    /**
+     * Resetea a su estado inicial el juego y rellena la matriz con su correspondiente numero de imagen
+     */
     public void iniciarJuego() {
         vidas = 3;
         fallos = 0;
@@ -40,29 +50,33 @@ public class ConcentreseJuego {
         matrizDestapar = new boolean[4][4];
         casillaDestapada = false;
 
-        int imagen = 8;
+        int imagen = 1;
         Random aleatorio = new Random();
         ArrayList<Integer> elementosColocados = new ArrayList<Integer>();
+        
+        //Ciclo que recorre todas la coordenadas de la matriz donde se almacenan el numero correspondiente a la imagen
         for (int x = 0; x < 4; x++) {
             for (int y = 0; y < 4; y++) {
+                //Genera un numero aleatorio y compara en el Array si no esta repetido mas de 2 veces
                 do {
-                    imagen = 1 + aleatorio.nextInt(8);
+                    imagen = 1 + aleatorio.nextInt(8);  //numero de imagen desde 1, hasta 8
                 } while (elementosColocados.indexOf(imagen) != elementosColocados.lastIndexOf(imagen));
-                elementosColocados.add(imagen);
+                
+                elementosColocados.add(imagen); //Agregamos el numero al Array para poder verificar que no se repita mas de 2 veces
                 matriz[x][y] = imagen;
             }
         }
     }
 
     /**
-     *
+     * Recibe las coordenadas de la casilla a destapar y actualiza su valor en matrizDestapar
      * @param x fila en la matriz
      * @param y columna en la matriz
      */
     public void destaparCasilla(int x, int y) {
-        if (!matrizDestapar[x][y]) { //ejecuta si la casilla esta en false (sin revelar)
+        if (!matrizDestapar[x][y]) { //Ejecuta si la casilla esta sin revelar (false)
             matrizDestapar[x][y] = true;
-            if (casillaDestapada) { //verifica si ya se ha destapado otra casilla
+            if (casillaDestapada) { //Verifica si ya se ha destapado otra casilla
                 casillaDestapada2X = x;
                 casillaDestapada2Y = y;
                 parCasillasDestapadas = true;
@@ -75,17 +89,17 @@ public class ConcentreseJuego {
     }
 
     /**
-     *
+     * Verifica si la casilla esta tapada o destapada
      * @param x fila en la matriz
      * @param y columna en la matriz
-     * @return false - imagen si revelar, true - imagen revelada
+     * @return false - imagen sin revelar, true - imagen revelada
      */
     public boolean estadoCasilla(int x, int y) {
         return matrizDestapar[x][y];
     }
 
     /**
-     *
+     * Retorna el numero de la imagen correspondiente a las coordenadas de la casilla
      * @param x fila en la matriz
      * @param y columna en la matriz
      * @return el numero de la imagen
@@ -94,12 +108,15 @@ public class ConcentreseJuego {
         return matriz[x][y];
     }
 
+    /**
+     * Compara las 2 casillas ya destapadas y las deja destapadas si son iguales, o las tapa si son diferentes, ademas que actualiza los parametros del juego
+     */
     public void compararCasillas() {
-        if (matriz[casillaDestapada1X][casillaDestapada1Y] == matriz[casillaDestapada2X][casillaDestapada2Y]) { //si tienen la misma imagen, quedan reveladas
+        if (matriz[casillaDestapada1X][casillaDestapada1Y] == matriz[casillaDestapada2X][casillaDestapada2Y]) { //Si tienen la misma imagen, quedan destapadas
             matrizDestapar[casillaDestapada1X][casillaDestapada1Y] = true;
             matrizDestapar[casillaDestapada2X][casillaDestapada2Y] = true;
             aciertos++;
-        } else { //se voltean las dos casillas
+        } else { //Se tapan las dos casillas
             matrizDestapar[casillaDestapada1X][casillaDestapada1Y] = false;
             matrizDestapar[casillaDestapada2X][casillaDestapada2Y] = false;
             vidas--;
@@ -109,10 +126,18 @@ public class ConcentreseJuego {
         parCasillasDestapadas = false;
     }
 
+    /**
+     * Verifica si ya se han seleccionado 2 casillas diferentes
+     * @return true - si ya se ha seleccionado y destapado 2 casillas diferentes
+     */
     public boolean getParCasillasDestapadas() {
         return parCasillasDestapadas;
     }
 
+    /**
+     * Pone en valor verdadero (true) a toda la matriz "matrizDestapar"
+     * que corresponde a que todas las casillas estan destapadas
+     */
     public void destaparTodasCasillas() {
         for (int x = 0; x < 4; x++) {
             for (int y = 0; y < 4; y++) {
@@ -121,15 +146,20 @@ public class ConcentreseJuego {
         }
     }
 
+    /**
+     * Pone en el valor inicial (false) a toda la matriz "matrizDestapar"
+     * que corresponde a que todas las casillas estan tapadas
+     */
     public void taparTodasCasillas() {
         matrizDestapar = new boolean[4][4];
-        for (int x = 0; x < 4; x++) {
-            for (int y = 0; y < 4; y++) {
-                matrizDestapar[x][y] = false;
-            }
-        }
     }
 
+    /**
+     * 
+     * @return 0 - Si ya no quedan vidas (perdio el juego)
+     * 1 - Si obtuvo los 8 acierto (gano el juego)
+     * 2 - Continua con el juego
+     */
     public int getEstadoJuego() {
         if (vidas == 0) {
             juegosTotales++;
@@ -145,6 +175,10 @@ public class ConcentreseJuego {
         return 2;
     }
 
+    /**
+     * Informacion acerca de como se debe jugar
+     * @return Texto de ayuda sobre la jugabilidad del juego
+     */
     public String mostrarAyuda() {
         return "CONCENTRESE\n" + "Saludos invocador, "
                 + "Primero debe seleccionar la tematica del juego y presionar el botón de Iniciar Juego,\n"
@@ -153,8 +187,12 @@ public class ConcentreseJuego {
                 + "Mucha suerte! c:";
     }
 
+    /**
+     * Informacion acerca de los juegos realizados mientras la aplicacion estuvo abierta
+     * @return Texto sobre las estadisticas de los juegos ganados y perdidos
+     */
     public String mostrarEstadisticas() {
-        if (juegosTotales != 0) {
+        if (juegosTotales != 0) {   //Solo muestra las estadisticas si se ha completado almenos un juego
             double porcentajeGanados = (double) juegosGanados / juegosTotales * 100;
             double porcentajePerdidos = (double) (juegosTotales - juegosGanados) / juegosTotales * 100;
             return "Estadisticas de Jugadas\n\n"
@@ -165,4 +203,5 @@ public class ConcentreseJuego {
             return "No se han completado partidas";
         }
     }
+    
 }
